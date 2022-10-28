2eef72a862f56d9fdfb4553a5c6f199061e45ac8
Quinn Culver <qculver@hpu.edu>
2022-10-27 16:37:01 -1000

    what's this message gonna do

--- src/Ch2.lean
+++ src/Ch2.lean
@@ -54,3 +54,42 @@ def curried_add_projs: ℕ → ℕ → ℕ := λ x y, x+y
 def curry_N (f : ℕ × ℕ → ℕ) : ℕ → ℕ → ℕ := λ a, (λ b, f(a,b))
 #check curry_N
 #reduce curry_N add_projs
+
+
+#check list.nil
+#check list.cons
+#check list.append
+
+/- Above, we used the example vec α n for vectors of elements of type α of length n. Declare a constant vec_add that could represent a function that adds two vectors of natural numbers of the same length, and a constant vec_reverse that can represent a function that 
+reverses its argument. Use implicit arguments for parameters that can be inferred. Declare some variables and check some expressions involving the constants that you have declared. -/
+-- namespace hidden
+universe u
+constant vec : Type u → ℕ → Type u
+constant empt : Π α : Type u, vec α 0
+constant cons :
+  Π (α : Type u) (n : ℕ), α → vec α n → vec α (n + 1)
+constant vec_add : Π {α : Type u} {n : ℕ}, vec α n → vec α n → vec α n
+
+constant vec_reverse : Π {α: Type u} {n: ℕ}, vec α n → vec α n
+
+constant v : vec ℕ 2
+#check @v
+#check v
+#check vec_add v v
+#check vec_reverse v
+#check vec_reverse (vec_add v v)
+#check vec_add v (vec_reverse v)  
+-- end hidden
+
+-- universe u
+
+
+/- 4. Similarly, declare a constant matrix so that matrix α m n could represent the type of m by n matrices. Declare some constants to represent functions on this type, such as matrix addition and multiplication, and (using vec) multiplication of a matrix by a vector. Once again, declare some variables and check some expressions involving the constants that you have declared. -/
+
+constant matrix : Type u → ℕ → ℕ → Type u
+
+constant mat_mul: Π {α : Type u} {m : ℕ} {n : ℕ} {k : ℕ}, matrix α m n → matrix α n k → matrix α m k
+#check mat_mul
+
+constant mat_vec_mul: Π {α : Type u} {m : ℕ} {n: ℕ} matrix α m n → vec α n → vec α m
+
---
+++ src/Ch3.lean
@@ -0,0 +1,45 @@
+constants p q : Prop
+
+theorem t1 : p → q → p := λ hp : p, λ hq : q, hp
+#print t1
+#check t1
+#reduce t1
+
+theorem t2 : p → q → p :=
+begin
+  intros h g,
+  show p, 
+  from h,
+  --apply h,
+end
+
+
+variables p q r : Prop
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := 
+example : p ∨ q ↔ q ∨ p := sorry
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ false ↔ p := sorry
+example : p ∧ false ↔ false := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+
+#check true 
+#check
